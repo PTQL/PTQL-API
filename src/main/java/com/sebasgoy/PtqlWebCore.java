7e1055e82991deba765b3f09e53746d2f90d5056
SebitasGS12 <sebasgoy16@gmail.com>
2024-07-18 22:43:19 -0600

    cambio a yml y nombre app

--- src/main/java/com/sebasgoy/PtqlWebCore.java
+++ src/main/java/com/sebasgoy/PtqlWebCore.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PtqlWebCore {
+	
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PtqlWebCore.class, args);
 		System.out.println("Tu aplicación Spring se ha iniciado correctamente.");
-		System.out.println("Puedes acceder a ella en: http://localhost:9090/");
+		System.out.println("Puedes acceder a ella en: http://localhost:8080/");
 	}
 }
